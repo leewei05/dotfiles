babd57a52725cd4c4931cee420384f28e882ce90
Lee <lee10202013@gmail.com>
2022-07-17 11:43:17 +0800

    Install nvim

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,16 +1,8 @@
-" Plugins will be downloaded under the specified directory.
-call plug#begin('~/.vim/plugged')
+call plug#begin()
 
 " Declare the list of plugins.
-Plug 'tpope/vim-sensible'
 Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
 Plug 'preservim/nerdtree'
-Plug 'tpope/vim-fugitive'
-Plug 'vim-airline/vim-airline'
-Plug 'ycm-core/YouCompleteMe'
-Plug 'rakr/vim-one'
-Plug 'vim-scripts/c.vim'
 
-" List ends here. Plugins become visible to Vim after this call.
 call plug#end()
 
--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ COLOR_RED="\033[1;31m"
 COLOR_PURPLE="\033[1;35m"
 COLOR_YELLOW="\033[1;33m"
 COLOR_NONE="\033[0m"
+OS="`uname`"
+NVIM=$HOME/.config/nvim
 
 info() {
     echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
@@ -19,14 +21,18 @@ title() {
 }
 
 get_linkables() {
-		find "$PWD" -maxdepth 1 -type f -not -name "*.sh"
+	find "$PWD" -maxdepth 1 -type f -not -name "*.sh"
+}
+
+get_nvim_configs() {
+    find "$PWD/.config/nvim" -maxdepth 1 -type f 
 }
 
 setup_symlinks() {
     title "Creating symlinks"
 
-		for file in $(get_linkables) ; do
-				echo "$file"
+	for file in $(get_linkables) ; do
+		echo "$file"
         target="$HOME/$(basename "$file")"
         if [ -e "$target" ]; then
             info "~${target#$HOME} already exists... Skipping."
@@ -37,31 +43,87 @@ setup_symlinks() {
     done
 }
 
+setup_git() {
+    title "Installing and setting up git"
+
+    info "Install git"
+    case "$OS" in
+        Linux)
+            sudo apt install git-all
+            ;;
+        Darwin)
+            xcode-select --install
+            ;;
+    esac
+    info "Setup git"
+}
+
 setup_fzf() {
     title "Installing fzf"
 
-		if [ -d "$HOME/.fzf" ]; then
-				info "fzf exists, skipping installation"
-		else
-		    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-		    ~/.fzf/install --completion --update-rc --key-bindings
-		fi
+	if [ -d "$HOME/.fzf" ]; then
+		info "fzf exists, skipping installation"
+	else
+	    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	    ~/.fzf/install --completion --update-rc --key-bindings
+	fi
 }
 
 setup_fish() {
     title "Installing fish"
 
-		git clone https://github.com/fish-shell/fish-shell.git ~/.local/share/fish-shell
-		cd ~/.local/share/fish-shell; cmake .; make; sudo make install
+	if ! type "$foobar_command_name" > /dev/null; then
+		wget https://github.com/fish-shell/fish-shell/releases/download/3.5.0/fish-3.5.0.tar.xz 
+    fi
+	
+	git clone https://github.com/fish-shell/fish-shell.git ~/.local/share/fish-shell
+	cd ~/.local/share/fish-shell; cmake .; make; sudo make install
+}
+
+setup_nvim() {
+    title "Install neovim"
+
+    info "Install vim-plug"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+    info "Install neovim on $OS"
+    case "$OS" in
+        Linux)
+            sudo apt-get install neovim
+            ;;
+        Darwin)
+            brew install neovim
+            ;;
+    esac
+
+    info "Creating $NVIM"
+    mkdir -p $NVIM
+
+    info "Creating symlinks"
+    for file in $(get_nvim_configs) ; do
+		echo "$file"
+        target="$NVIM/$(basename "$file")"
+        if [ -e "$target" ]; then
+            info "~${target#$NVIM} already exists... Skipping."
+        else
+            info "Creating symlink for $file"
+            ln -s "$file" "$target"
+        fi
+    done
 }
 
 case "$1" in
     link)
         setup_symlinks
         ;;
-		all)
-				setup_symlinks
-				setup_fzf
-				setup_fish
-				;;
+    nvim)
+        setup_nvim
+        ;;
+	all)
+		setup_symlinks			
+        setup_fzf	
+        setup_fish
+        setup_nvim
+		;;
 esac
