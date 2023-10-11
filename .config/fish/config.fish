f179483faa605c97f9a93ec5bf9b5df10e6a3303
Lee <lee10202013@gmail.com>
2023-10-11 10:57:46 +0800

    Update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
-set PATH /usr/local/bin /usr/sbin /usr/local/texlive/2022/bin/universal-darwin $HOME/bin $HOME/.cargo/bin $PATH
+set PATH /usr/local/bin /usr/local/opt/llvm/bin /usr/sbin /usr/local/texlive/2022/bin/universal-darwin $HOME/bin $HOME/.cargo/bin $PATH
 
 abbr -a vim nvim
+abbr -a d nvim /Users/lee/dev/
